68942c7b5b8b666c6a919becbed4f2536595880e
Scott Frase <frases@hotmail.com>
2025-11-16 08:45:10 -0500

    Require to require_once change

--- ansible/roles/docker/files/apache/webroot/src/index.php
+++ ansible/roles/docker/files/apache/webroot/src/index.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Production\Api\Controllers\UploadController;
 use Production\Api\Infrastructure\Database;
