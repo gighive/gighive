6bfa405f9954dd5177b1d310258433d41843e274
Scott Frase <frases@hotmail.com>
2025-11-16 08:38:45 -0500

    remove test file

--- ansible/roles/docker/files/apache/webroot/src/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simple test script to verify /src routing works
-echo "SUCCESS: /src/test.php is working!\n";
-echo "REQUEST_URI: " . ($_SERVER['REQUEST_URI'] ?? 'null') . "\n";
-echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? 'null') . "\n";
-echo "PATH_INFO: " . ($_SERVER['PATH_INFO'] ?? 'null') . "\n";
-echo "Time: " . date('Y-m-d H:i:s') . "\n";
-?>
